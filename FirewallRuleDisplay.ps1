1e7c9c4b37e8f74010cfbaf923ace2790b458cdb
Dave Cushing <dave.cushing@cambriancollege.ca>
2021-10-29 10:29:23 -0400

    Update FirewallRuleDisplay.ps1

--- FirewallRuleDisplay.ps1
+++ FirewallRuleDisplay.ps1
@@ -3,7 +3,7 @@ Get-NetFirewallRule -DisplayName $rulename |
 Format-Table DisplayName,
 @{Name='Protocol';Expression={($PSItem | Get-NetFirewallPortFilter).Protocol}},
 @{Name='Program';Expression={($PSItem | Get-NetFirewallApplicationFilter).Program}},
-@{Name='Program';Expression={($PSItem | Get-NetFirewallPortFilter).LocalPort}},
+@{Name='LocalPort';Expression={($PSItem | Get-NetFirewallPortFilter).LocalPort}},
 @{Name='RemotePort';Expression={($PSItem | Get-NetFirewallPortFilter).RemotePort}},
 @{Name='RemoteAddress';Expression={($PSItem | Get-NetFirewallAddressFilter).RemoteAddress}},
 Enabled,
