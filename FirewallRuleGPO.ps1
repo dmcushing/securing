cb3a318f499c19920aabd781982b650b3fd05713
Dave Cushing <dave.cushing@cambriancollege.ca>
2021-10-30 08:05:49 -0400

    Create FirewallRuleGPO.ps1

---
+++ FirewallRuleGPO.ps1
@@ -0,0 +1,10 @@
+Get-NetFirewallRule -PolicyStore ActiveStore -PolicyStoreSourceType GroupPolicy |
+Format-Table DisplayName,
+@{Name='Protocol';Expression={($PSItem | Get-NetFirewallPortFilter).Protocol}},
+@{Name='Program';Expression={($PSItem | Get-NetFirewallApplicationFilter).Program}},
+@{Name='Program';Expression={($PSItem | Get-NetFirewallPortFilter).LocalPort}},
+@{Name='RemotePort';Expression={($PSItem | Get-NetFirewallPortFilter).RemotePort}},
+@{Name='RemoteAddress';Expression={($PSItem | Get-NetFirewallAddressFilter).RemoteAddress}},
+Enabled,
+Direction,
+Action
