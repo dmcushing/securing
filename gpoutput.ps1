6a485669e8780fb463e55efc5e34359f055af2d8
Dave Cushing <dave.cushing@cambriancollege.ca>
2021-11-08 08:38:50 -0500

    Create gpoutput.ps1

---
+++ gpoutput.ps1
@@ -0,0 +1,17 @@
+$gponame=$args[0]
+# Get the GPO Guid
+$Id = (Get-GPO -DisplayName $gponame).Id
+# Store the output in a (XML) variable
+[xml]$GpoXml = Get-GPOReport -Guid $Id -ReportType Xml
+
+#Create a custom object containing only the policy "fields" we're interested in
+$PolicyDetails = foreach ($p in $GpoXml.GPO.User.ExtensionData.Extension.Policy) {
+    [PSCustomObject]@{
+        "Name" = $p.Name
+        "State" = $p.State
+        "Supported" = $p.Supported
+    }
+}
+
+#Let's see the results
+$PolicyDetails
